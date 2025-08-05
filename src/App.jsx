d55cc66db7d07e87cc41dec31c60bceed8470dc7
fitahiana Dimbiniaina <blackbooty956@gmail.com>
2025-08-05 20:48:49 +0300

    adding new skills

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import { Projects } from "./components/Projects";
 import { Contact } from "./components/Contact";
 import { Footer } from "./components/Footer";
 import Service from './components/Services';
-import Techexp from './components/TecheXP';
+import Techexp from './components/Tech/TecheXP';
 
 function App() {
   return (
