47c739a2b440283acdc705f7e07dd14f844d2f6b
fitahiana Dimbiniaina <blackbooty956@gmail.com>
2025-08-05 20:02:57 +0300

    adding resume

---
+++ src/components/TecheXP.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { FaDocker } from "react-icons/fa";
+import './TechXP.css';
+const TecheXP = () => {
+  return (
+    <div className="techexp-wrapper">
+      <div className="techexp-card">
+        <h2 className="techexp-title">Other Technical Experience</h2>
+
+        <div className="techexp-content">
+          <FaDocker size={48} className="techexp-icon" />
+          <div>
+            <h3 className="techexp-subtitle">Docker Compose for API Management</h3>
+            <p className="techexp-text">    
+            Configured and managed multi-container Docker Compose environments to run Gravitee API management platform locally, enabling rapid development and testing of microservices architectures.      
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TecheXP;
